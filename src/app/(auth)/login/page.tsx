570ee2d5531eab8ab8e998049b4f3bf20d2a1917
Soe Htun <shtun106@gmail.com>
2025-02-23 14:57:08 +0700

    first

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// () means route groups, you can put name within (), 
+// you can test in browser like /login, not auth/login
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function BarChart() {
+    return <h1>Bar Chart</h1>
+}
+export default function Dashboard() {
+  return (
+    <div>Dashboard</div>
+  )
+}
